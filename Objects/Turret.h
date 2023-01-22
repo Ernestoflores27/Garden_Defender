b9f7e956dcd81d496f129007698f9479015d4066
maxilar20 <mael.dorne@outlook.com>
2023-01-22 15:30:37 +0000

    Added classes for detector and turret

---
+++ Objects/Turret.h
@@ -0,0 +1,25 @@
+using namespace std;
+
+class Turret
+{
+public:
+    Turret()
+    {
+    }
+
+    void MoveTurret()
+    {
+    }
+    void MoveHorizontal()
+    {
+    }
+    void MoveVertical()
+    {
+    }
+    void Shoot()
+    {
+    }
+    void Explore()
+    {
+    }
+};
