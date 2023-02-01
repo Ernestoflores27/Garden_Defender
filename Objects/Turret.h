e3981d1ea9cee256615dd43297e634b0633808ad
maxilar20 <dorne99@hotmail.com>
2023-02-01 14:48:35 +0000

    GPIO control

--- Objects/Turret.h
+++ Objects/Turret.h
@@ -1,10 +1,16 @@
+#include <pigpio.h>
+
 using namespace std;
+
+#define LED_PIN 6
 
 class Turret
 {
 public:
     Turret()
     {
+        gpioInitialise();
+        gpioSetMode(LED_PIN, PI_OUTPUT);
     }
     void MoveTurret()
     {
@@ -17,6 +23,13 @@ public:
     }
     void Shoot()
     {
+        while (true)
+        {
+            gpioDelay(100000);
+            gpioWrite(LED_PIN, 1);
+            gpioDelay(100000);
+            gpioWrite(LED_PIN, 0);
+        }
     }
     void Explore()
     {
--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 #include "Objects/Detector.h"
 #include <opencv4/opencv2/opencv.hpp>
 
+#include <pigpiod_if2.h>
+
+using namespace std;
+
 int main()
 {
+    int GPIO = 18;
+    int level;
+    if (gpioInitialise() < 0)
+        return 1;
+    gpioWrite(GPIO, 1);
+    level = gpioRead(GPIO);
+    printf("GPIO %d is %d\n", GPIO, level);
+    gpioTerminate();
+
     Camera cam1(320, 240);
     VideoCapture real_time = cam1.getVideoCapture();
 
