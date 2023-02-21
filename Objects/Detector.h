641a1f0791cf4ea7208929dd2c387ac8e403593e
Ernestoflores27 <ernesto_fp_97@hotmail.com>
2023-02-21 16:06:43 +0000

    Removed face detection

--- Objects/Detector.h
+++ Objects/Detector.h
@@ -41,6 +41,7 @@ public:
 
 	Net net;
 	vector<Mat> detections;
+	Mat img;
 
 	Detector(VideoCapture real_time_)
 	{
@@ -53,25 +54,17 @@ public:
 		}
 		net = readNet("Garden_Defender/yolov5s.onnx");
 
-		string trained_classifier_location = "Garden_Defender/haarcascade_frontalface_default.xml";
-		faceDetector.load(trained_classifier_location);
-
-		namedWindow("Face Detection");
+		namedWindow("Object Detection");
 	}
 
 	void draw_label(Mat &input_image, string label, int left, int top)
 	{
-		// Display the label at the top of the bounding box.
 		int baseLine;
 		Size label_size = getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
 		top = max(top, label_size.height);
-		// Top left corner.
 		Point tlc = Point(left, top);
-		// Bottom right corner.
 		Point brc = Point(left + label_size.width, top + label_size.height + baseLine);
-		// Draw white rectangle.
 		rectangle(input_image, tlc, brc, BLACK, FILLED);
-		// Put the label on the black rectangle.
 		putText(input_image, label, Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
 	}
 
@@ -93,7 +86,7 @@ public:
 	{
 		real_time.read(video_stream);
 		detections = pre_process(video_stream, net);
-		Mat img = post_process(video_stream.clone(), detections, class_list);
+		img = post_process(video_stream.clone(), detections, class_list);
 
 		vector<double> layersTimes;
 		double freq = getTickFrequency() / 1000;
@@ -101,10 +94,94 @@ public:
 		string label = format("Inference time : %.2f ms", t);
 
 		putText(img, label, Point(20, 40), FONT_FACE, FONT_SCALE, RED);
-		imshow("Output", img);
 		return detections;
 	}
 
+	vector<Mat> pre_process(Mat &input_image, Net &net)
+	{
+		Mat blob;
+		blobFromImage(input_image, blob, 1. / 255., Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(), true, false);
+
+		net.setInput(blob);
+
+		vector<Mat> outputs;
+		net.forward(outputs, net.getUnconnectedOutLayersNames());
+
+		return outputs;
+	}
+
+	Mat post_process(Mat input_image, vector<Mat> &outputs, const vector<string> &class_name)
+	{
+		vector<int> class_ids;
+		vector<float> confidences;
+		vector<Rect> boxes;
+		float x_factor = input_image.cols / INPUT_WIDTH;
+		float y_factor = input_image.rows / INPUT_HEIGHT;
+		float *data = (float *)outputs[0].data;
+		const int dimensions = 85;
+		const int rows = 25200;
+		for (int i = 0; i < rows; ++i)
+		{
+			float confidence = data[4];
+			if (confidence >= CONFIDENCE_THRESHOLD)
+			{
+				float *classes_scores = data + 5;
+				Mat scores(1, class_name.size(), CV_32FC1, classes_scores);
+				Point class_id;
+				double max_class_score;
+				minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
+				if (max_class_score > SCORE_THRESHOLD)
+				{
+					confidences.push_back(confidence);
+					class_ids.push_back(class_id.x);
+					float cx = data[0];
+					float cy = data[1];
+					float w = data[2];
+					float h = data[3];
+					int left = int((cx - 0.5 * w) * x_factor);
+					int top = int((cy - 0.5 * h) * y_factor);
+					int width = int(w * x_factor);
+					int height = int(h * y_factor);
+					boxes.push_back(Rect(left, top, width, height));
+				}
+			}
+			data += 85;
+		}
+		vector<int> indices;
+		NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, indices);
+		for (int i = 0; i < indices.size(); i++)
+		{
+			int idx = indices[i];
+			Rect box = boxes[idx];
+			int left = box.x;
+			int top = box.y;
+			int width = box.width;
+			int height = box.height;
+			rectangle(input_image, Point(left, top), Point(left + width, top + height), BLUE, 3 * THICKNESS);
+			string label = format("%.2f", confidences[idx]);
+			label = class_name[class_ids[idx]] + ":" + label;
+			draw_label(input_image, label, left, top);
+		}
+		return input_image;
+	}
+
+	void Show()
+	{
+		imshow("Object Detection", img);
+	}
+
+	void sortFaces()
+	{
+		sort(faces_vector.begin(), faces_vector.end());
+	}
+	int getOffsetX()
+	{
+		return faces_vector[0].offset_x;
+	}
+	int getOffsetY()
+	{
+		return faces_vector[0].offset_y;
+	}
 	void drawBoundaries()
 	{
 		int i = 0;
@@ -131,127 +208,6 @@ public:
 	// 	line(video_stream, Point(real_time.get(CAP_PROP_FRAME_WIDTH) / 2, 0), Point(real_time.get(CAP_PROP_FRAME_WIDTH) / 2, real_time.get(CAP_PROP_FRAME_HEIGHT)), Scalar(0, 0, 255), 1);
 	// 	line(video_stream, Point(0, real_time.get(CAP_PROP_FRAME_HEIGHT) / 2), Point(real_time.get(CAP_PROP_FRAME_WIDTH), real_time.get(CAP_PROP_FRAME_HEIGHT) / 2), Scalar(0, 0, 255), 1);
 	// }
-	void draw_label_(Mat &input_image, string label, int left, int top)
-	{
-		// Display the label at the top of the bounding box.
-		int baseLine;
-		Size label_size = getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
-		top = max(top, label_size.height);
-		// Top left corner.
-		Point tlc = Point(left, top);
-		// Bottom right corner.
-		Point brc = Point(left + label_size.width, top + label_size.height + baseLine);
-		// Draw white rectangle.
-		rectangle(input_image, tlc, brc, BLACK, FILLED);
-		// Put the label on the black rectangle.
-		putText(input_image, label, Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
-	}
-
-	vector<Mat> pre_process(Mat &input_image, Net &net)
-	{
-		// Convert to blob.
-		Mat blob;
-		blobFromImage(input_image, blob, 1. / 255., Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(), true, false);
-
-		net.setInput(blob);
-
-		// Forward propagate.
-		vector<Mat> outputs;
-		net.forward(outputs, net.getUnconnectedOutLayersNames());
-
-		return outputs;
-	}
-
-	Mat post_process(Mat input_image, vector<Mat> &outputs, const vector<string> &class_name)
-	{
-		// Initialize vectors to hold respective outputs while unwrapping detections.
-		vector<int> class_ids;
-		vector<float> confidences;
-		vector<Rect> boxes;
-		// Resizing factor.
-		float x_factor = input_image.cols / INPUT_WIDTH;
-		float y_factor = input_image.rows / INPUT_HEIGHT;
-		float *data = (float *)outputs[0].data;
-		const int dimensions = 85;
-		// 25200 for default size 640.
-		const int rows = 25200;
-		// Iterate through 25200 detections.
-		for (int i = 0; i < rows; ++i)
-		{
-			float confidence = data[4];
-			// Discard bad detections and continue.
-			if (confidence >= CONFIDENCE_THRESHOLD)
-			{
-				float *classes_scores = data + 5;
-				// Create a 1x85 Mat and store class scores of 80 classes.
-				Mat scores(1, class_name.size(), CV_32FC1, classes_scores);
-				// Perform minMaxLoc and acquire the index of best class  score.
-				Point class_id;
-				double max_class_score;
-				minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
-				// Continue if the class score is above the threshold.
-				if (max_class_score > SCORE_THRESHOLD)
-				{
-					// Store class ID and confidence in the pre-defined respective vectors.
-					confidences.push_back(confidence);
-					class_ids.push_back(class_id.x);
-					// Center.
-					float cx = data[0];
-					float cy = data[1];
-					// Box dimension.
-					float w = data[2];
-					float h = data[3];
-					// Bounding box coordinates.
-					int left = int((cx - 0.5 * w) * x_factor);
-					int top = int((cy - 0.5 * h) * y_factor);
-					int width = int(w * x_factor);
-					int height = int(h * y_factor);
-					// Store good detections in the boxes vector.
-					boxes.push_back(Rect(left, top, width, height));
-				}
-			}
-			// Jump to the next row.
-			data += 85;
-		}
-		// Perform Non-Maximum Suppression and draw predictions.
-		vector<int> indices;
-		NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, indices);
-		for (int i = 0; i < indices.size(); i++)
-		{
-			int idx = indices[i];
-			Rect box = boxes[idx];
-			int left = box.x;
-			int top = box.y;
-			int width = box.width;
-			int height = box.height;
-			// Draw bounding box.
-			rectangle(input_image, Point(left, top), Point(left + width, top + height), BLUE, 3 * THICKNESS);
-			// Get the label for the class name and its confidence.
-			string label = format("%.2f", confidences[idx]);
-			label = class_name[class_ids[idx]] + ":" + label;
-			// Draw class labels.
-			draw_label_(input_image, label, left, top);
-		}
-		return input_image;
-	}
-
-	void Show()
-	{
-		imshow("Face Detection", video_stream);
-	}
-
-	void sortFaces()
-	{
-		sort(faces_vector.begin(), faces_vector.end());
-	}
-	int getOffsetX()
-	{
-		return faces_vector[0].offset_x;
-	}
-	int getOffsetY()
-	{
-		return faces_vector[0].offset_y;
-	}
 	// void lineClosest()
 	// {
 	// 	line(video_stream, Point(real_time.get(CAP_PROP_FRAME_WIDTH) / 2, real_time.get(CAP_PROP_FRAME_HEIGHT) / 2), Point(faces_vector[0].center_x, faces_vector[0].center_y), Scalar(0, 255, 0), 2);
