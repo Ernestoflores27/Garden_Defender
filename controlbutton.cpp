d4b4e0b609630291d455022134f14d9caa1f8858
Mingjiao <m18217287945@163.com>
2023-04-02 21:18:13 +0100

    control function

--- controlbutton.cpp
+++ controlbutton.cpp
@@ -1,9 +1,18 @@
+#include <opencv4/opencv2/opencv.hpp>
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
 #include <cstdio>
 #include <unistd.h>
 #include <fcgi_stdio.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+#include "Garden_Defender/Objects/Camera.h"
+#include "Garden_Defender/Objects/Detector.h"
+
+
 int main( int argc, char *argv[] )
 {
   while( FCGI_Accept() >= 0 )
@@ -58,12 +67,40 @@ int main( int argc, char *argv[] )
         }
         else if(strcmp(buf, "Automatic")==0)
         {
+            ofstream outfile("record.txt");
+            outfile << "1";
+            outfile.close();
+            //automatic
+            //MoveServo->autometic();
+            cv::Mat video_stream;
+            Camera cam1(640, 480);
+            cv::VideoCapture real_time = cam1.getVideoCapture();
+
+            Turret turret(23);
+            turret.changePosition(0, 0);
+            turret.moveT();
+
+            Detector detector_model("Garden_Defender/yoloFastestV2.onnx", 0.3, 0.4, 0.4, real_time, &turret);
+            detector_model.detectT();
+
+            do
+            {
+                std::cout << '\n'
+                          << "Press a Enter to close...";
+
+            } while (std::cin.get() != '\n');
+
+            real_time.release();
+            turret.release();
             FCGI_printf( "Status: 200 OK\r\n" );
             FCGI_printf( "Content-Type: text/html\r\n\r\n" );
-            FCGI_printf( "dowAutomaticn" );
+            FCGI_printf( "Automatic" );
         }
         else if(strcmp(buf, "Manual")==0)
         {
+            ofstream outfile("record.txt");
+            outfile << "0";
+            outfile.close();
             FCGI_printf( "Status: 200 OK\r\n" );
             FCGI_printf( "Content-Type: text/html\r\n\r\n" );
             FCGI_printf( "Manual" );
